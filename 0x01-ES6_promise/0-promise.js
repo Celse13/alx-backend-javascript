7d8bfb86785ab87f8d59dd71cde56af8b400c59e
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-06 20:41:52 +0200

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      let statusCode = 200;
-      setTimeout(() => {
-        if (statusCode === 200) {
-          resolve('Success');
-        } else {
-          reject(Error(`Failed status: ${statusCode}`));
-        }
-      }, 2000);
-    });
+  return new Promise((resolve, reject) => {
+    let status;
+    setTimeout(() => {
+      if (status === 200) {
+        resolve('Success');
+      } else {
+        reject(Error(`Failed status: ${status}`));
+      }
+    }, 2000);
+  });
 }
