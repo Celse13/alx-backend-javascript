593f653f169a9a8632a314b50da335d39ea4afa7
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-07 22:56:38 +0200

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,14 +1,30 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  get floors() { return this._floors; }
+  getFloors() { return this.floors; }
+
+  setFloors(floors) {
+    if (typeof floors !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+    this.floors = floors;
+  }
+
+  get sqft() { return this._sqft; }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new Error('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
