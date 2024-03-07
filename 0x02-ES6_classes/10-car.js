c0e26ffb573dd7bcff1ec970a5e0fa35a0ad29af
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-07 23:09:35 +0200

    Create 10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,38 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() { return this._brand; }
+
+  set brand(newBrand) {
+    if (typeof newBrand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    this._brand = newBrand;
+  }
+
+  get motor() { return this._motor; }
+
+  set motor(newMotor) {
+    if (typeof newMotor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    this._motor = newMotor;
+  }
+
+  get color() { return this._color; }
+
+  set color(newColor) {
+    if (typeof newColor !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
