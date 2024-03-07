80efeabd975ee402ecaa084fa216cc78368120d8
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-07 23:12:36 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,21 +1,21 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
     this._range = range;
   }
 
-  get range() { return this._range; }
-
-  set range(newRange) {
-    if (typeof newRange !== 'string') {
-      throw new TypeError('Range must be a string');
-    }
-    this._range = newRange;
-  }
-
   cloneCar() {
-    return new Car();
+    const Species = super.constructor[Symbol.species];
+    return new Species();
   }
 }
