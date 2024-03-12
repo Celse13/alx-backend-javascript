f14525d773e80b8256ee0cde9d20fc158805cafa
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-12 21:41:13 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
-function updateStudentGradeByCity(studentList, city, newGrades) {
-    return studentList
-        .filter(student => student.location === city)
-        .map(student => {
-            const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-            return {
-                ...student,
-                grade: gradeObj ? gradeObj.grade : 'N/A'
-            };
-        });
-}
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students.filter((student) => student.location === city).map((student) => {
+      const gradeObj = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+      if (gradeObj[0]) {
+        return {...student, grade: gradeObj[0].grade}
+        }
+        return { ...student, grade: gradeObj[0].grade };
+    });
+  }
