5b96436c1d89ed061d6c5013f48bab656798a5f4
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-12 22:20:12 +0200

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
-export default function updateUniqueItems(object) {
-    if (!(object instanceof Map)) throw Error('Cannot process');
-    for (const [key, val] of object) {
-      if (val === 1) { obj.set(key, 100); }
-    }
+export default function updateUniqueItems(obj) {
+  if (!(obj instanceof Map)) throw Error('Cannot process');
+  for (const [key, val] of obj) {
+    if (val === 1) { obj.set(key, 100); }
   }
+}
