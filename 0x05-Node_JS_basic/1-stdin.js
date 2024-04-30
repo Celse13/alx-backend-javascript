fb4bccfa1d9b56972320d3d90d99ad35bf3f289f
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-05-01 00:28:39 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,11 @@
-const readline = require('readline');
+process.stdin.setEncoding('utf-8');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
