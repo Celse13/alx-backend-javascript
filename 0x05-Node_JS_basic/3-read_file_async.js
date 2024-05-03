7d3b556c3d15d7f599a500d95742e571c116e1d9
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-05-03 18:32:24 +0200

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
-const countStudents = async (p) => {
+const countStudents = async (path) => {
   try {
-    const d = await fs.promises.readFile(p, 'utf8');
-    const r = d.split('\n');
-    const c = r.map(row => row.split(','));
-    const dt = c.slice(1, -1);
+    const csvData = await fs.promises.readFile(path, 'utf8');
+    const rows = csvData.split('\n');
+    const cols = rows.map((row) => row.split(','));
+    const data = cols.slice(1, -1);
 
-    const n = dt.length;
-    const cs = [];
-    const swe = [];
+    const numberOfStudents = data.length;
+    const csStudentsName = [];
+    const sweStudentsName = [];
 
-    for (const row of dt) {
+    for (const row of data) {
       if (row[3] === 'CS') {
-        cs.push(row[0]);
+        csStudentsName.push(row[0]);
       } else if (row[3] === 'SWE') {
-        swe.push(row[0]);
+        sweStudentsName.push(row[0]);
       }
     }
-    console.log(`Number of students: ${n}`);
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    console.log(`Number of students: ${numberOfStudents}`);
+    console.log(`Number of students in CS: ${csStudentsName.length}. List: ${csStudentsName.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudentsName.length}. List: ${sweStudentsName.join(', ')}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
