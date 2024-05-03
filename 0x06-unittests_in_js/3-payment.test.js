a863e3882bf14f48076f3701a64c91505fb9bae2
celse13 <rugiracelse13@gmail.com>
2024-05-03 04:54:07 +0200

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,10 +7,9 @@ describe('sendPaymentRequestToApi', function() {
   it('should call Utils.calculateNumber with "SUM", 100, and 20', function() {
 
     const spy = sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(100, 20);
 
-    sendPaymentRequestToApi(100, 20);
-
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
-    spy.restore();
+		expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+		spy.restore();
   });
 });
