7fc43c7f61f03fb374ee73a2f33c16bad5aa62db
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-05-03 19:25:52 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,62 +1,30 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8')
-      .then((data) => {
-        const lines = data.split('\n');
-        const hashtable = {};
-        let students = -1;
-        let result = '';
-        for (const line of lines) {
-          if (line.trim() !== '') {
-            const columns = line.split(',');
-            const field = columns[3];
-            const firstname = columns[0];
-            if (students >= 0) {
-              if (!Object.hasOwnProperty.call(hashtable, field)) {
-                hashtable[field] = [];
-              }
-              hashtable[field] = [...hashtable[field], firstname];
-            }
-            students += 1;
-          }
-        }
-        result += `Number of students: ${students}\n`;
-        for (const key in hashtable) {
-          if (Object.hasOwnProperty.call(hashtable, key)) {
-            result += `Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}\n`;
-          }
-        }
-        resolve(result);
-      })
-      .catch(() => {
-        reject(new Error('Cannot load the database'));
-      });
-  });
-}
-
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.writeHead(200);
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.writeHead(200);
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((error) => {
-        res.writeHead(404);
-        res.end(`This is the list of our students\n${error.message}`);
-      });
+    const dbPath = process.argv[2];
+    try {
+      const data = await fs.readFile(dbPath, 'utf8');
+      const lines = data.split('\n').filter(line => line);
+      const students = lines.slice(1); // Remove header
+      const csStudents = students.filter(student => student.split(',')[3] === 'CS');
+      const sweStudents = students.filter(student => student.split(',')[3] === 'SWE');
+      const responseText = `This is the list of our students\nNumber of students: ${students.length}\n` +
+        `Number of students in CS: ${csStudents.length}. List: ${csStudents.map(s => s.split(',')[0]).join(', ')}\n` +
+        `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(s => s.split(',')[0]).join(', ')}`;
+      res.end(responseText);
+    } catch (error) {
+      res.end(`Error: ${error.message}`);
+    }
   } else {
-    res.writeHead(404);
-    res.end('Not foundx');
+    res.statusCode = 404;
+    res.end('Not found');
   }
 });
 
 app.listen(1245);
-
 module.exports = app;
