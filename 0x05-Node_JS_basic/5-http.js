01f2d4f381488a37302dd78e2921e1daed4eb205
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-05-03 19:16:12 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,26 +1,26 @@
-const httpLib = require('http');
+const http = require('http');
 
 const countStudents = require('./3-read_file_async');
 
-const filePath = process.argv[2];
+const file = process.argv[2];
 
-const server = httpLib.createServer(async (request, response) => {
-  if (request.url === '/') {
-    response.write('Hello Holberton School!');
-    return response.end();
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    return res.end();
   }
-  if (request.url === '/students') {
-    response.write('This is the list of our students\n');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
     try {
-      const stuList = await countStudents(filePath);
-      return response.end(`${stuList.join('\n')}`);
-    } catch (e) {
-      response.end(e.message);
+      const students = await countStudents(file);
+      return res.end(`${students.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
     }
   }
-  return response.end();
+  return res.end();
 });
 
-server.listen(1245);
+app.listen(1245);
 
-module.exports = server;
+module.exports = app;
